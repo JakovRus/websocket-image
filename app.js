58a95edef95291b980ed5409ba1dab363cb8087c
Yakov Rogov <rogov@haulmont.com>
2019-10-13 11:29:08 +0400

    added function to convert base64 string to blob; it is required for getting the size of the downloaded image;

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static('public'));
+
 io.on('connection', function(socket){
   fs.readFile(__dirname + '/public/images/iguana.jpg', function(err, data){
-    socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"));
+    socket.emit('imageConversionByServer', data.toString("base64"));
   });
 });
 
